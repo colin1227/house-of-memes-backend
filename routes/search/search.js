93dfdf9170362878604ebe0d6ee516894b25ef20
Colin Daniel <colin.daniel1227@gmail.com>
2021-11-24 20:54:52 -0500

    search Bar added

--- routes/search/search.js
+++ routes/search/search.js
@@ -1,18 +1,38 @@
 const express = require("express");
 const cors = require('cors');
-const { hashTagsTermQuery } = require("../../db/queries");
+const { hashTagsTermQuery, memeSearchQuery } = require("../../db/queries");
 
 var corsOptions = {
   origin: '*',
   optionsSuccessStatus: 200
 };
 
-const { hashTagsTermQuery } = require('../../db/queries');
-
-
 const router = express.Router();
 
-router.get('/hashtags', async(req, res, next) => {
+router.get('/meme/:term', async(req, res, next) => {
+  try {
+    const modifiedSearchTerm = req.params.term.toLowerCase();
+    const query = await memeSearchQuery(modifiedSearchTerm);
+    // TODO: weblinks added to search
+    const options = query.rows.map(row => {
+      return {
+        label: row.description,
+        awsName: row.aws_name,
+        format: row.format
+      }
+    });
+    res.json({
+        options
+      });
+  } catch(err) {
+    next(err);
+    res.json({
+      error: err.message
+    })
+  }
+})
+
+router.get('/hashtags/:term', async(req, res, next) => {
   try {
 
     const hashTagsTermQuery = await hashTagsTermQuery(req.query.term);
